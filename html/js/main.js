78e11f883dd3016e3a9037b8dec32740da6373c4
Michael Hill <bitfixer@bitfixer.com>
2022-07-23 12:09:36 -0700

    use grayscale value in detection

--- html/js/main.js
+++ html/js/main.js
@@ -15,11 +15,13 @@ video.style.height = '1px';
 
 const testdiv = document.querySelector('#testdiv');
 var captureInterval = setInterval(captureImage, 33);
-var pixRegion = new Uint8Array(glyphDim * glyphDim);
-var pix_arr = new Uint8Array(canvas.width * canvas.height);
+var pixRegion = new Float32Array(glyphDim * glyphDim);
+//var pix_arr = new Uint8Array(canvas.width * canvas.height);
 var glyphResult = new Uint8Array(1000);
-var glyphBrightness = new Uint32Array(256);
+var glyphBrightness = new Float32Array(256);
 //var checkGlyph = new Uint8Array(256);
+//var brightnessTable = new Float32Array(16777216);
+
 
 function getGlyphBrightness() {
     var g = 0;
@@ -36,6 +38,28 @@ function getGlyphBrightness() {
         glyphBrightness[g] = brightness;
     }
 }
+
+/*
+function makeBrightnessTable() {
+    // prepare lookup table of all possible grayscale values
+    var r;
+    var g;
+    var b;
+    var index = 0;
+    for (r = 0; r < 256; r++)
+    {
+        for (g = 0; g < 256; g++)
+        {
+            for (b = 0; b < 256; b++)
+            {
+                var br = Math.sqrt(r*r + g*g + b*b);
+                brightnessTable[index] = br;
+                //console.log(r + " " + g + " " + b + " " + br + " " + brightnessTable[index]);
+            }
+        }
+    }
+}
+*/
 
 function captureImage() {
     clearInterval(captureInterval);
@@ -66,6 +90,7 @@ function captureImage() {
     var perr = 0;
     var glyphError = 0;
     var glyphResultIndex = 0;
+    var imageIndex = 0;
     for (y = 0; y < canvas.height; y += glyphDim)
     {
         for (x = 0; x < canvas.width; x += glyphDim)
@@ -76,11 +101,64 @@ function captureImage() {
             // copy block into pixel region
             for (yy = 0; yy < 8; yy++)
             {
+                imageIndex = pind + (yy*canvas.width*4);
                 for (xx = 0; xx < 8; xx++)
                 {
-                    pixRegion[regionIndex] = imdata.data[pind + (yy*canvas.width*4) + xx*4];
+                    //pixRegion[regionIndex] = imdata.data[pind + (yy*canvas.width*4) + xx*4];
+                    //pixRegion[regionIndex] = imdata.data[imageIndex + (xx*4) + 1];
+
+                    /*
+                var b = Math.sqrt(
+                            imdata.data[imageIndex]*imdata.data[imageIndex] +
+                            imdata.data[imageIndex+1]*imdata.data[imageIndex+1] +
+                            imdata.data[imageIndex+2]*imdata.data[imageIndex+2]);
+                pixRegion[regionIndex] = b;
+                imageIndex = imageIndex + 4;
+                */
+                    /*
+                    var rr = imdata.data[imageIndex];
+                    var gg = imdata.data[imageIndex+1];
+                    var bb = imdata.data[imageIndex+2];
+                    imageIndex += 4;
+
+                    var b = brightnessTable[rr*65536 + gg*256 + bb];
+                    pixRegion[regionIndex] = b;
+                    */
+
+                    //pixRegion[regionIndex] = imdata.data[imageIndex + (xx*4)];
+
+                    /*
+                    var b = Math.sqrt(
+                            imdata.data[imageIndex]*imdata.data[imageIndex] +
+                            imdata.data[imageIndex+1]*imdata.data[imageIndex+1] +
+                            imdata.data[imageIndex+2]*imdata.data[imageIndex+2]);
+                            */
+
+                    //var b = 0.3 * imdata.data[imageIndex] + 0.59 * imdata.data[imageIndex+1] + 0.11 * imdata.data[imageIndex+2]
+
+                    var b = (imdata.data[imageIndex] + imdata.data[imageIndex+1] + imdata.data[imageIndex+2]) / 3.0;
+
+                    if (b >= 255.0)
+                    {
+                        console.log("oops! " + b + " " +imageIndex+ " " + imdata.data[imageIndex] + " " + imdata.data[imageIndex+1] + " " + imdata.data[imageIndex+2]);
+                    }
+
+                    imageIndex = imageIndex + 4;
+                    pixRegion[regionIndex] = b;
+
                     regionBrightness += pixRegion[regionIndex];
                     regionIndex++;
+
+                    /*
+                    var b = Math.sqrt(
+                            imdata.data[imageIndex]*imdata.data[imageIndex] +
+                            imdata.data[imageIndex+1]*imdata.data[imageIndex+1] +
+                            imdata.data[imageIndex+2]*imdata.data[imageIndex+2]);
+                    pixRegion[regionIndex] = b;
+                    regionBrightness += b;
+                    regionIndex++;
+                    imageIndex += 4;
+                    */
                 }
             }
 
@@ -141,4 +219,5 @@ function handleError(error) {
 }
 
 getGlyphBrightness();
+//makeBrightnessTable();
 navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
