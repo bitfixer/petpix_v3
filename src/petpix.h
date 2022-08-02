492f37d7dff8196fbfe56001ade28321baa34190
Michael Hill <bitfixer@bitfixer.com>
2022-08-02 01:11:17 -0700

    changed gpio pins

--- src/petpix.h
+++ src/petpix.h
@@ -7,12 +7,12 @@
 #define OUTPUT_ENABLE_BAR   38
 #define DATA0               36
 #define DATA1               32
-#define DATA2               37
+#define DATA2               28
 #define DATA3               35
 #define DATA4               33
 #define DATA5               31
 #define DATA6               29
-#define DATA7               27
+#define DATA7               37
 
 #define CA1                  16 // handshake line TO pet
 #define CB2                  18 // handshake line FROM pet
