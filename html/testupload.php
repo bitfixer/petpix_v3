157be99a0307e263ede2211d047c51426f207914
Michael Hill <bitfixer@bitfixer.com>
2022-08-04 15:55:30 -0700

    fix path on testupload.php

--- html/testupload.php
+++ html/testupload.php
@@ -1,4 +1,4 @@
 <?php
-	file_put_contents('/home/pi/petpix_images/test.img', file_get_contents('php://input'));
-	shell_exec("cat /home/pi/petpix_images/test.img | nc -N 127.0.0.1 9600");
+	file_put_contents('/mnt/tmp/test.img', file_get_contents('php://input'));
+	shell_exec("cat /mnt/tmp/test.img | nc -N 127.0.0.1 9600");
 ?>
