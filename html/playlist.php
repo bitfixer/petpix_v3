d9d513ddb6be90f3c8b284e3db1506c80b4e6c92
Michael Hill <bitfixer@bitfixer.com>
2022-09-04 00:21:19 -0700

    playlist writing

---
+++ html/playlist.php
@@ -0,0 +1,27 @@
+<?php
+    $payload = file_get_contents('php://input');
+    $playlist_file = fopen("control/playlist.txt", "w");
+    fwrite($playlist_file, $payload);
+    fclose($playlist_file);
+
+    // write control byte
+    $control_byte = 1;
+    $control_file = fopen("control/p.ctl", "r");
+    if ($control_file)
+    {
+        $byte = fread($control_file, 1);
+        $control_byte = ord($byte);
+        $control_byte += 1;
+        if ($control_byte > 255)
+        {
+            $control_byte = 1;
+        }
+        fclose($control_file);
+    }
+    $control_file = fopen("control/p.ctl", "w");
+    $bs = pack("C", $control_byte);
+    fwrite($control_file, $bs);
+    fclose($control_file);
+
+    printf("Playing");
+?>
